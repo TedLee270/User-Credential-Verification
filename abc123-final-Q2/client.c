61bb1555f7eba1ec5288f48f6df0946b839238fb
Yunseong Lee <yunseonglee15@gmail.com>
2024-11-30 19:30:45 -0600

    Update client.c
    Updated client.c with the given requirements

--- abc123-final-Q2/client.c
+++ abc123-final-Q2/client.c
@@ -1,28 +1,45 @@
-/* client.c - code for example client program that uses TCP */
+/* client.c - Modified code for assignment */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <time.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>  // Include for gethostbyname()
 #include <unistd.h>
 
+// Including the header file for the struct
+#include "sha256_lib.h"
 
-int     clientSocket;    /* socket descriptor                   */ 
-int     port;            /* protocol port number                */  
+
+int clientSocket;    /* socket descriptor */ 
+int port;            /* protocol port number */  
+
+// Function to hash input using SHA-256
+void sha256_hash(const char *input, char *hash) {
+  SHA256_CTX ctx;
+  uint8_t hash_bytes[SHA256_DIGEST_SIZE];
+
+  sha256_init(&ctx);
+  sha256_update(&ctx, (uint8_t *)input, strlen(input));
+  sha256_final(&ctx, hash_bytes);
+  // Convert the hash bytes to a hexadecimal string
+  for (int i = 0; i < SHA256_DIGEST_SIZE; i++) {
+    sprintf(hash + (i * 2), "%02x", hash_bytes[i]);
+  }
+}
 
 int main(int argc, char *argv[])
 { 
-  struct  sockaddr_in sad; /* structure to hold an IP address     */
-  struct  hostent  *ptrh;  /* pointer to a host table entry       */
-  char    *host;           /* pointer to host name                */
+  struct sockaddr_in sad; /* structure to hold an IP address     */
+  struct hostent  *ptrh;  /* pointer to a host table entry       */
+  char *host;           /* pointer to host name                */
   
-  char    Sentence[128]; 
-  char    modifiedSentence[128]; 
-  char    buff[128];
-  int     n;
+  char Sentence[128]; 
+  char modifiedSentence[128]; 
+  char buff[128];
+  int n;
   
   if (argc != 3) {
     fprintf(stderr,"Usage: %s server-name port-number\n",argv[0]);
@@ -60,30 +77,100 @@ int main(int argc, char *argv[])
     exit(1);
   }
   
-  /* Read a sentence from user */
-  
-  printf("Sentence   :   "); 
-  fgets(Sentence, 127, stdin);
-  
-  /* Send the sentence to the server  */
-  
-  write(clientSocket, Sentence, strlen(Sentence)+1);
-  
-  /* Get the modified sentence from the server and write it to the screen*/
-  modifiedSentence[0]='\0';
-  n=read(clientSocket, buff, sizeof(buff));
-  while(n > 0){
-    strncat(modifiedSentence,buff,n);
-    if (buff[n-1]=='\0') break;
-    n=read(clientSocket, buff, sizeof(buff));
+  // Main loop for the client
+  while(1) {
+    char choice;
+    char user_email[128];
+    char user_password[128];
+    char hashed_user_email[SHA256_BLOCK_SIZE * 2 + 1];
+    char hashed_password[SHA256_BLOCK_SIZE * 2 + 1];
+    clock_t start, stop;
+    double elapsed_time;
+
+    printf("Enter an option:\n");
+    printf("1: Check username or email\n");
+    printf("2: Check password\n");
+    printf("3: Check both username/email and password\n");
+    printf("4: Exit\n");
+    scanf(" %c", &choice);
+
+    switch (choice) { // Change to if statements too
+      case '1':
+        printf("Enter your username or email: ");
+        scanf("%s", user_email);
+
+        start = clock(); // Begin timer
+        sha256_hash(user_email, hashed_user_email);
+        stop = clock(); // End timer
+
+        // Calculate elapsed time
+        elapsed_time = ((double)(stop - start)) / CLOCKS_PER_SEC;
+        
+        // Send the hashed email to the server
+        send(clientSocket, hashed_user_email, strlen(hashed_user_email), 0);
+
+        // Receive the response from the server
+        n = recv(clientSocket, buff, sizeof(buff), 0);
+        buff[n] = '\0'; // Null-terminate the received data
+        printf("Server reply: %s\n", buff);
+        printf("Elapsed time: %lf seconds\n", elapsed_time);
+        break;
+      case '2':
+        printf("Enter your password: ");
+        scanf(" %s", user_password);
+
+        start = clock(); // Begin timer
+        sha256_hash(user_password, hashed_password);
+        stop = clock(); // End timer
+
+        // Calculate elapsed time
+        elapsed_time = ((double)(stop - start)) / CLOCKS_PER_SEC;
+
+        // Send the hashed password to the server
+        send(clientSocket, hashed_password, strlen(hashed_password), 0);
+
+        // Receive the response from the server
+        n = recv(clientSocket, buff, sizeof(buff), 0);
+        buff[n] = '\0'; // Null-terminate the received data
+        printf("Server reply: %s\n", buff);
+        printf("Elapsed time: %lf seconds\n", elapsed_time);
+        break;
+      case '3':
+        printf("Enter your username or email: ");
+        scanf("%s", user_email);
+        sha256_hash(user_email, hashed_user_email);
+        
+        printf("Enter your password: ");
+        scanf(" %s", user_password);
+        sha256_hash(user_password, hashed_password);
+
+        start = clock(); // Begin timer
+        // Sending both the hashed username/email and password to the server
+        send(clientSocket, hashed_user_email, strlen(hashed_user_email), 0);
+        send(clientSocket, hashed_password, strlen(hashed_password), 0);
+        stop = clock(); // End timer
+
+        // Calculate elapsed time
+        elapsed_time = ((double)(stop - start)) / CLOCKS_PER_SEC;
+
+        // Receive the response from the server
+        n = recv(clientSocket, buff, sizeof(buff), 0);
+        buff[n] = '\0'; // Null-terminate the received data
+        printf("Server reply: %s\n", buff);
+        printf("Elapsed time: %lf seconds\n", elapsed_time);
+        break;
+      case '4':
+        printf("Thank you for using the program! Now exiting...\n");
+        send(clientSocket, "exit", strlen("exit"), 0); // Sending exit message
+        close(clientSocket);
+        return 0;
+      default:
+        printf("Not a valid option. Please try again.\n");
+        break;
+    }
   }
   
-  printf("Modified to -> %s\n",modifiedSentence);
-
   /* Close the socket. */
-  
   close(clientSocket);
   return 0;  
 }
-
-
